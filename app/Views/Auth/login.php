9217d0b692cc08a0612140a5b9e4492cb17d19a4
iriando <taufik.iriando@gmail.com>
2025-11-16 15:00:28 +0900

    login-page change

--- app/Views/Auth/login.php
+++ app/Views/Auth/login.php
@@ -8,70 +8,47 @@
   <link rel="stylesheet" href="<?= base_url('adminlte/plugins/fontawesome-free/css/all.min.css') ?>">
   <link rel="stylesheet" href="<?= base_url('adminlte/plugins/icheck-bootstrap/icheck-bootstrap.min.css') ?>">
   <link rel="stylesheet" href="<?= base_url('adminlte/dist/css/adminlte.min.css') ?>">
+  <link rel="stylesheet" href="<?= base_url('animation/style-login.css') ?>">
 </head>
 <body class="hold-transition login-page">
 
-<div class="login-box">
-  <!-- Logo -->
-  <div class="login-logo">
-    <a href="#"><b>SiMAPAN</b></a>
+  <div class="login-box">
+    <!-- Logo -->
+    <div class="login-logo">
+      <a href="#"><b>SiMAPAN</b></a>
+    </div>
   </div>
 
-  <!-- Card -->
-  <div class="card">
-    <div class="card-body login-card-body">
-      <p class="login-box-msg">Login</p>
-
-      <!-- Form Login -->
-      <form action="<?= url_to('login') ?>" method="post">
+    <form action="<?= url_to('login') ?>" method="post">
         <?= csrf_field() ?>
 
-        <!-- Email / Username -->
-        <div class="input-group mb-3">
-          <input type="text" name="login" class="form-control" placeholder="Email atau Username" value="<?= old('login') ?>" required autofocus>
-          <div class="input-group-append">
-            <div class="input-group-text"><span class="fas fa-user"></span></div>
+      <div class="box">
+        <div class="login">
+          <div class="loginBx">
+            <h2>
+              <i class="fa-solid fa-right-to-bracket"></i>
+              Login
+              <!-- <i class="fa-solid fa-heart"></i> -->
+            </h2>
+            <input type="text" name="login" placeholder="Email" value="<?= old('login') ?>">
+            <input type="password" name="password" placeholder="Password">
+            <input type="submit" value="Masuk" />
+            <!-- <div class="group">
+              <a href="#">Forgot Password</a>
+              <a href="#">Sign up</a>
+            </div> -->
           </div>
         </div>
-
-        <!-- Password -->
-        <div class="input-group mb-3">
-          <input type="password" name="password" class="form-control" placeholder="Password" required>
-          <div class="input-group-append">
-            <div class="input-group-text"><span class="fas fa-lock"></span></div>
-          </div>
-        </div>
-
-        <!-- Remember Me -->
-        <!-- <div class="row">
-          <div class="col-8">
-            <div class="icheck-primary">
-              <input type="checkbox" id="remember" name="remember">
-              <label for="remember">Ingat Saya</label>
-            </div>
-          </div> -->
-          <div class="col-4">
-            <button type="submit" class="btn btn-primary btn-block">Masuk</button>
-          </div>
-        </div>
-      </form>
-
+      </div>
+    </form>
+  <!-- </div> -->
+  
       <!-- Error Messages -->
       <?php if (session()->getFlashdata('error')): ?>
         <div class="alert alert-danger mt-2">
           <?= session()->getFlashdata('error') ?>
         </div>
       <?php endif; ?>
-
-      <!-- <p class="mb-1 mt-3">
-        <a href="<//?= url_to('forgot') ?>">Lupa Password?</a>
-      </p>
-      <p class="mb-0">
-        <a href="<//?= url_to('register') ?>" class="text-center">Daftar akun baru</a>
-      </p> -->
-    </div>
-  </div>
-</div>
 
 <!-- AdminLTE JS -->
 <script src="<?= base_url('adminlte/plugins/jquery/jquery.min.js') ?>"></script>
