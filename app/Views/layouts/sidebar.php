143850d340347a7dc59ef943cdad856009d8cf6e
iriando <taufik.iriando@gmail.com>
2025-11-25 16:39:29 +0900

    deploy fix

--- app/Views/layouts/sidebar.php
+++ app/Views/layouts/sidebar.php
@@ -13,30 +13,30 @@
         <!-- Layanan -->
         <li class="nav-header">Layanan</li>
 
-        <!-- <?php if (in_groups(['admin', 'petugas BMN'])): ?>
+        <?php if (in_groups(['admin', 'petugas BMN'])): ?>
         <li class="nav-item">
           <a href="<?= base_url('peminjaman') ?>" class="nav-link">
             <i class="nav-icon fas fa-handshake"></i>
             <p>Peminjaman</p>
           </a>
-        </li> -->
+        </li>
 
-        <!-- <li class="nav-item">
+        <li class="nav-item">
           <a href="<?= base_url('distribusiatk') ?>" class="nav-link">
             <i class="nav-icon fas fa-parachute-box"></i>
             <p>Distribusi ATK / ARTK</p>
           </a>
         </li>
-        <?php endif; ?> -->
+        <?php endif; ?>
 
-        <!-- <?php if (in_groups(['admin', 'petugas BMN', 'petugas klinik' ])): ?>
+        <?php if (in_groups(['admin', 'petugas BMN', 'petugas klinik' ])): ?>
         <li class="nav-item">
           <a href="<?= base_url('distribusiobat') ?>" class="nav-link">
             <i class="nav-icon fas fa-prescription-bottle"></i>
             <p>Distribusi Obat-obatan</p>
           </a>
         </li>
-        <?php endif; ?> -->
+        <?php endif; ?>
 
         <?php if (in_groups(['admin', 'petugas BMN'])): ?>
         <li class="nav-item">
@@ -47,15 +47,15 @@
         </li>
         <?php endif; ?>
         
-        <!-- <li class="nav-item">
+        <li class="nav-item">
           <a href="<?= base_url('logpegawai') ?>" class="nav-link">
             <i class="nav-icon fas fa-running"></i>
             <p>Log Pegawai</p>
             <span class="badge badge-danger navbar-badge">Beta</span>
           </a>
-        </li> -->
+        </li>
 
-        <!-- <?php if (in_groups(['admin', 'petugas BMN'])): ?>
+        <?php if (in_groups(['admin', 'petugas BMN'])): ?>
           <li class="nav-header">Klinik</li>
           <li class="nav-item">
             <a href="<?= base_url('rekammedis') ?>" class="nav-link">
@@ -63,18 +63,18 @@
               <p>Rekam Medis</p>
             </a>
           </li>
-        <?php endif ?> -->
+        <?php endif ?>
 
         <!-- Asset -->
         <?php if (in_groups(['admin', 'petugas BMN'])): ?>
         <li class="nav-header">Aset</li>
 
-        <!-- <li class="nav-item">
+        <li class="nav-item">
           <a href="<?= base_url('pegawai') ?>" class="nav-link">
             <i class="nav-icon fas fa-child"></i>
             <p>Pegawai</p>
           </a>
-        </li> -->
+        </li>
         
         <li class="nav-item">
           <a href="<?= base_url('barang') ?>" class="nav-link">
@@ -83,23 +83,23 @@
           </a>
         </li>
 
-        <!-- <li class="nav-item">
+        <li class="nav-item">
           <a href="<?= base_url('atk') ?>" class="nav-link">
             <i class="nav-icon fas fa-pencil-alt"></i>
             <p>ATK / ARTK</p>
           </a>
-        </li> -->
+        </li>
 
-        <!-- <li class="nav-item">
+        <li class="nav-item">
           <a href="<?= base_url('obat') ?>" class="nav-link">
             <i class="nav-icon fas fa-pills"></i>
             <p>Obat</p>
           </a>
-        </li> -->
+        </li>
         <?php endif; ?>
 
         <!-- Pengaturan -->
-        <!-- <?php if (in_groups('admin')): ?>
+        <?php if (in_groups('admin')): ?>
         <li class="nav-header">Pengaturan</li>
 
         <li class="nav-item">
@@ -114,7 +114,7 @@
             <p>Template Manager</p>
           </a>
         </li>
-        <?php endif; ?> -->
+        <?php endif; ?>
 
       </ul>
     </nav>
